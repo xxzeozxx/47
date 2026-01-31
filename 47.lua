e3e0362194a99815bcd6f7c83e2d340bafd62621
xxzeozxx <ddghazyal@gmail.com>
2026-01-31 12:35:22 +0700

    Update 47.lua

--- 47.lua
+++ 47.lua
@@ -95,7 +95,7 @@ local SecretList = {
     "Ancient Lochness Monster", "Talon Serpent", "Hacker Shark", "ElRetro Gran Maja",
     "Strawberry Choc Megalodon", "Krampus Shark", "Emerald Winter Whale",
     "Winter Frost Shark", "Icebreaker Whale", "Leviathan", "Pirate Megalodon", "Viridis Lurker",
-    "Cursed Kraken", "Jellyfish",
+    "Cursed Kraken", "Sapphyra",
 }
 
 local StoneList = { "Ruby" }
